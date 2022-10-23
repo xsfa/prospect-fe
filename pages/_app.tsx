d736dd70aedfba9522503384c676626e1a4d53f6
xsfa <tesfaaog@gmail.com>
2022-10-22 17:17:23 -0700

    auth

---
+++ pages/_app.tsx
@@ -0,0 +1,23 @@
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
+import { useState } from 'react'
+import { AppProps } from 'next/app'
+
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{
+  initialSession: Session,
+}>) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient())
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  )
+}
+export default MyApp
